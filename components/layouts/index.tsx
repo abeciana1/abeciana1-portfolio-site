c41c8a8865248cbbdabf0a49724bcfb8179ba293
Alex <alex.beciana@gmail.com>
2022-11-08 17:14:30 -0500

    Created export of page margin comp

---
+++ components/layouts/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const PageMargin = ({ children }: any) => {
+
+    return (
+        <section className="px-5 md:px-10 py-4 mx-auto page-margin">
+            {children}
+        </section>
+    )
+}

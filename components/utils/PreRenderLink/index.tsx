1fce367de0e7e62359722f66cc2b0593838abbf3
Alex <alex.beciana@gmail.com>
2022-11-13 14:33:24 -0500

    Removed comment and extra repeated code in classname styling

--- components/utils/PreRenderLink/index.tsx
+++ components/utils/PreRenderLink/index.tsx
@@ -32,7 +32,6 @@ interface LinkBtnI {
     ctaButtonColor: string;
 }
 
-// focus:ring-altBlue focus:ring-4
 export const PreRenderLinkAsBtn = ({
     href,
     linkText,
@@ -42,13 +41,13 @@ export const PreRenderLinkAsBtn = ({
     return (
         <Link
             href={href}
-            className={cx("px-4 py-2 rounded-md",{
-                ["text-xl xl:text-2xl leading-10 xl:leading-loose bg-altYellow text-black"]: ctaButtonColor === "altYellow",
-                ["text-xl xl:text-2xl leading-10 xl:leading-loose bg-black text-white"]: ctaButtonColor === "black",
-                ["text-xl xl:text-2xl leading-10 xl:leading-loose bg-altRed text-white"]: ctaButtonColor === "altRed",
-                ["text-xl xl:text-2xl leading-10 xl:leading-loose bg-altGray text-black"]: ctaButtonColor === "altGray",
-                ["text-xl xl:text-2xl leading-10 xl:leading-loose bg-altBlue text-white"]: ctaButtonColor === "altBlue",
-                ["text-xl xl:text-2xl leading-10 xl:leading-loose bg-blue-800 text-white"]: ctaButtonColor === "blue-800"
+            className={cx("text-xl xl:text-2xl leading-10 xl:leading-loose px-4 py-2 rounded-md",{
+                ["bg-altYellow text-black"]: ctaButtonColor === "altYellow",
+                ["bg-black text-white"]: ctaButtonColor === "black",
+                ["bg-altRed text-white"]: ctaButtonColor === "altRed",
+                ["bg-altGray text-black"]: ctaButtonColor === "altGray",
+                ["bg-altBlue text-white"]: ctaButtonColor === "altBlue",
+                ["bg-blue-800 text-white"]: ctaButtonColor === "blue-800"
             })}
         >
             { linkText }

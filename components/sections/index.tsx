eb0bc19dcfdb9a31f539a507dbff52f76b233595
Alex <alex.beciana@gmail.com>
2022-11-12 22:34:24 -0500

    Created initial render of HeroSection comp

--- components/sections/index.tsx
+++ components/sections/index.tsx
@@ -1,2 +1,23 @@
 
+interface HeroSectionI {
+    heading: string;
+    taglineBody: string;
+    image: string;
+    cta?: string;
+    ctaButtonColor?: string;
+}
 
+export const HeroSection = ({
+    heading,
+    taglineBody,
+    image,
+    cta,
+    ctaButtonColor
+}: HeroSectionI) => {
+
+    return(
+        <section>
+            <section></section>
+        </section>
+    )
+}

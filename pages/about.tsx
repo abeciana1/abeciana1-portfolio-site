2c47a5e3747eace9b89edcf6090d21138c82ceea
Alex <alex.beciana@gmail.com>
2022-11-15 22:49:27 -0500

    Added custom head comp for about page && Started About page construction

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,10 +1,18 @@
 import React from 'react'
-
+import CustomHead from '../components/utils/CustomHead'
+import { PageMargin } from '../components/layouts'
+import { HeroSectionWithLinkGradientBG } from '../components/sections'
 
 const AboutPage = () => {
     return (
         <React.Fragment>
-
+            <CustomHead
+                title="About"
+                description="Alex Beciana | Work experience and education as a digital marketer, community manager, and full-stack software engineer"
+            />
+            <PageMargin>
+                
+            </PageMargin>
         </React.Fragment>
     )
 }

725445892e509aec8dd631a0305bc86fc8330523
Alex <alex.beciana@gmail.com>
2022-11-21 22:07:37 -0500

    Created initial rendering of education card

---
+++ components/cards/EduCard/index.tsx
@@ -0,0 +1,26 @@
+
+
+interface EduCardI {
+    id: number;
+    schoolName: string;
+    schoolWebsite: string;
+    schoolImage: string;
+    achievements: string;
+}
+
+const EduCard = ({
+    id,
+    schoolName,
+    schoolWebsite,
+    schoolImage,
+    achievements
+}: EduCardI) => {
+
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default EduCard

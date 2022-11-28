e51fad292c0c32f6e98a22bd5a199cc861deb281
Alex <alex.beciana@gmail.com>
2022-11-27 20:58:23 -0500

    Added CustomHead comp import

--- pages/blog/[slug].tsx
+++ pages/blog/[slug].tsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import { PageMargin } from '../../components/layouts'
+import CustomHead from '../../components/utils/CustomHead'
 
 const BlogArticle = () => {
 
     return (
         <React.Fragment>
             <PageMargin>
-                
+
             </PageMargin>
         </React.Fragment>
     )

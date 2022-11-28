0d934448a45dc34c60ddf1cc1d51f78a5f0cdb4b
Alex <alex.beciana@gmail.com>
2022-11-27 20:02:31 -0500

    Created initial render of blog article page

---
+++ pages/blog/[slug].tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { PageMargin } from '../../components/layouts'
+
+const BlogArticle = () => {
+
+    return (
+        <React.Fragment>
+            <PageMargin>
+                
+            </PageMargin>
+        </React.Fragment>
+    )
+}
+
+export default BlogArticle

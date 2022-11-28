4193f7961e6b5123f711ae5f5cbcccfd5cc6367e
Alex <alex.beciana@gmail.com>
2022-11-27 20:00:37 -0500

    Added custom head reneder and page margin to blog index page

--- pages/blog/index.tsx
+++ pages/blog/index.tsx
@@ -1,8 +1,20 @@
-
+import React from 'react'
+import CustomHead from '../../components/utils/CustomHead'
+import { PageMargin } from '../../components/layouts'
 
 const BlogPage = () => {
 
     return (
-        
+        <React.Fragment>
+            <CustomHead
+                title="Blog"
+                description="Alex Beciana | Writing and sharing software engineering concepts in easy-to-understand articles and tutorials."
+            />
+            <PageMargin>
+                
+            </PageMargin>
+        </React.Fragment>
     )
 }
+
+export default BlogPage

6272eebecff3e75e5e75861375cab695d9c083df
Alex <alex.beciana@gmail.com>
2022-12-22 18:21:55 -0500

    Created initial render of project card comp

--- components/cards/ProjectCard/index.tsx
+++ components/cards/ProjectCard/index.tsx
@@ -3,3 +3,14 @@ import Link from 'next/link'
 import moment from 'moment';
 import { Transition } from '@headlessui/react'
 import { TagCard, TagI } from '../TagCard'
+
+const ProjectCard = () => {
+
+    return (
+        <React.Fragment>
+
+        </React.Fragment>
+    )
+}
+
+export default ProjectCard

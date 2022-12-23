7c6639b0cd7016c4aa75e4c19ea7ed75e359e362
Alex <alex.beciana@gmail.com>
2022-12-22 19:11:22 -0500

    Finished creating notion project helper funcs

--- lib/notion-proj-props.ts
+++ lib/notion-proj-props.ts
@@ -6,3 +6,29 @@ export const getClientInfo = (project: any | undefined) => {
         clientLogo: ""
     }
 }
+
+export const getProjectTitle = (project: any | undefined) => {
+    return project.properties.projectTitle.title[0].plain_text
+}
+
+export const getProjectStatus = (project: any | undefined) => {
+    return {
+        color: project.properties.status.multi_select[0].color,
+        name: project.properties.status.multi_select[0].name
+    }
+}
+
+export const getProjectTools = (project: any | undefined) => {
+    return project.properties.toolsUsed.multi_select
+}
+
+export const getClientType = (project: any | undefined) => {
+    return {
+        color: project.properties.type.select.color,
+        name: project.properties.type.select.name
+    }
+}
+
+export const getProjectWorkAreas = (project: any | undefined) => {
+    return project.properties.workAreas.multi_select
+}

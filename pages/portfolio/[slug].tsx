d6b976045a23284d7488dfa2692dce1ebf3ace00
Alex <alex.beciana@gmail.com>
2022-12-23 01:04:35 -0500

    Mapped over projets and returned paths

--- pages/portfolio/[slug].tsx
+++ pages/portfolio/[slug].tsx
@@ -1,4 +1,7 @@
 import React from 'react'
+import { GetStaticProps, GetStaticPaths } from 'next'
+import { getDatabase } from '../../lib/helper-functions'
+import { getProjectSlug } from '../../lib/notion-proj-props'
 
 const PortfolioProjectPage = () => {
 
@@ -10,3 +13,27 @@ const PortfolioProjectPage = () => {
 }
 
 export default PortfolioProjectPage
+
+export const getStaticPaths: GetStaticPaths = async (context: any) => {
+
+    const projects = await getDatabase(process.env.NOTION_PORTFOLIO_DATABASE_ID)
+
+    let paths = projects.map((project: any) => {
+        let slug = getProjectSlug(project)
+        return {
+            params: {
+                slug: slug
+            }
+        }
+    })
+    
+    return {
+        paths,
+        fallback: false
+    }
+}
+
+export const getStaticProps: GetStaticProps = async (context: any) => {
+
+    return {}
+}

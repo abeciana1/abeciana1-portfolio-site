545f521c58997f72cf2664d47781c0badf865a61
Alex <alex.beciana@gmail.com>
2022-12-22 19:48:27 -0500

    All project props mapped

--- lib/notion-proj-props.ts
+++ lib/notion-proj-props.ts
@@ -1,9 +1,9 @@
 export const getClientInfo = (project: any | undefined) => {
     return {
-        clientName: project.properties.clientName.rich_text[0].plain_text,
-        clientBio: project.properties.clientBio.rich_text[0].plain_text,
-        clientLink: project.properties.clientLink.url,
-        clientLogo: ""
+        name: project.properties.clientName.rich_text[0].plain_text,
+        bio: project.properties.clientBio.rich_text[0].plain_text,
+        link: project.properties.clientLink.url,
+        logo: project.properties.clientLogo.url
     }
 }
 

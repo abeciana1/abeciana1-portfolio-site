87b64ffa06089baa9f5cd338cec099b4b75dbec0
Alex <alex.beciana@gmail.com>
2022-12-24 00:17:35 -0500

    Added eslint disable next line

--- components/cards/JobCard/index.tsx
+++ components/cards/JobCard/index.tsx
@@ -74,14 +74,11 @@ const JobCard = ({
                     <ReactMarkdown
                         children={responsibilities}
                         components={{
-                            // eslint-disable-next-line
                             ul({ children }: any) {
-                                // eslint-disable-next-line
                                 return (
                                     <ul
                                         className="list-disc ml-5"
                                     >
-                                        {/* //eslint-disable-next-line  */}
                                         {children}
                                     </ul>
                                 )

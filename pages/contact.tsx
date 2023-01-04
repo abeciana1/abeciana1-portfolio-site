4777ac1fff717c07498145b72a610cc2e4604e9a
Alex <alex.beciana@gmail.com>
2023-01-04 01:41:16 -0500

    Rplaced script comp outside of head

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -2,20 +2,20 @@ import React from 'react'
 import { PageMargin } from '../components/layouts'
 import ContactForm from '../components/utils/_forms/ContactForm'
 import Image from 'next/image'
-import Head from 'next/head'
+// import Head from 'next/head'
 import Script from 'next/script'
 
 const Contact = () => {
 
     return (
         <React.Fragment>
-            <Head>
+            {/* <Head> */}
                 <Script
                     id="hs-script-loader"
                     src="//js.hs-scripts.com/23677902.js"
                     strategy="lazyOnload"
                 />
-            </Head>
+            {/* </Head> */}
             <PageMargin>
                 <section
                     id="contact"

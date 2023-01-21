ff9a50689018283e91945ab105f04698b19ec8ac
Alex <alex.beciana@gmail.com>
2023-01-21 15:18:19 -0500

    Removed 2 todo task items

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,10 +7,8 @@ import Head from 'next/head'
 import NavBar from '../components/utils/_nav/NavBar'
 import { StrictMode } from "react";
 
-// todo Add back to top button blog post article page
 // todo Add liking / claps feature
 // todo Add section for personal updates from me
-// todo Create smooth scroll comp for long form content
 
 export default function App({ Component, pageProps }: AppProps) {
 

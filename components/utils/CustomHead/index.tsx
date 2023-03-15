d488e7a1ed8b9924f11b62d467999bbb9c53a447
Alex <alex.beciana@gmail.com>
2023-03-15 18:42:21 -0400

    Added opengraph image to blog post head && Commented out json ld for blog posts

--- components/utils/CustomHead/index.tsx
+++ components/utils/CustomHead/index.tsx
@@ -1,5 +1,6 @@
 import { NextSeo } from 'next-seo'
 import { useRouter } from 'next/router'
+// import { NewsArticleJsonLd } from 'next-seo';
 
 interface SeoI {
     title?: string;
@@ -66,27 +67,42 @@ export const BlogPostHead = ({
     const router = useRouter()
 
     return (
-        <NextSeo
-            title={title}
-            titleTemplate='Alex Beciana | %s'
-            defaultTitle='Alex Beciana'
-            description={description}
-            canonical={"https://alexbeciana.com" + router.asPath}
-            openGraph={{
-                title: title,
-                description: description,
-                url: "https://alexbeciana.com" + router.asPath,
-                type: 'article',
-                article: article,
-                images: [
-                {
-                    url: image,
-                    width: 850,
-                    height: 650,
-                    alt: `Alex Beciana | ${title}`,
-                },
-                ],
-            }}
-        />
+        <>
+            {/* <NewsArticleJsonLd
+                url={"https://alexbeciana.com" + router.asPath}
+                title={title || ""}
+                description={description}
+                section="education"
+                keywords={article.blogTags}
+                datePublished={article?.publishedTime || ""}
+                authorName="Alex Beciana"
+                dateCreated={article?.publishedTime || ""}
+                publisherName="Alex Beciana"
+                publisherLogo="./profile-callout-edited.webp"
+
+            /> */}
+            <NextSeo
+                title={title}
+                titleTemplate='Alex Beciana | %s'
+                defaultTitle='Alex Beciana'
+                description={description}
+                canonical={"https://alexbeciana.com" + router.asPath}
+                openGraph={{
+                    title: title,
+                    description: description,
+                    url: "https://alexbeciana.com" + router.asPath,
+                    type: 'article',
+                    article: article,
+                    images: [
+                    {
+                        url: image,
+                        width: 850,
+                        height: 650,
+                        alt: `Alex Beciana | ${title}`,
+                    },
+                    ],
+                }}
+            />
+        </>
     )
 }

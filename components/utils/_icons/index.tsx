8a037c08d8e6789de56cd7e34a35947be6aa5913
Alex <alex.beciana@gmail.com>
2023-03-19 11:37:39 -0400

    Created initial render of ServiceIcon comp

---
+++ components/utils/_icons/index.tsx
@@ -0,0 +1,24 @@
+import { Fragment, useState } from 'react'
+import cx from 'classnames'
+
+interface IServiceIcon {
+    text: string;
+    color: string;
+    icon: React.ElementType;
+}
+
+export const ServiceIcon = ({
+    text,
+    color,
+    icon
+}: IServiceIcon) => {
+    const Icon = icon
+    const [hovered, setHovered] = useState(false)
+
+    return (
+        <Fragment>
+            <Icon
+            />
+        </Fragment>
+    )
+}

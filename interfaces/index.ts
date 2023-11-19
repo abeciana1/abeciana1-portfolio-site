1475548d1e1702d2afd65f3ca16fd9ac2332bd55
Alex <alex.beciana@gmail.com>
2023-11-19 17:54:58 -0500

    Exported interfaces

--- interfaces/index.ts
+++ interfaces/index.ts
@@ -1,1 +1,13 @@
-export type { PostI } from '@/interfaces/Blog'
+export type {
+    ITagData,
+    IPostData
+} from '@/interfaces/Data'
+export type {
+    IPost,
+    IPostImage,
+    IPostTag
+} from '@/interfaces/Blog'
+export type {
+    ITag,
+    IBlogPostCard
+} from '@/interfaces/Component'

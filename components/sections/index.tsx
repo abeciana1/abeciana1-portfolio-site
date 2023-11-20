95143d97a7557092b8c52e7867c9b175c713d0e6
Alex <alex.beciana@gmail.com>
2023-11-19 19:28:20 -0500

    Updated import to use @ syntax

--- components/sections/index.tsx
+++ components/sections/index.tsx
@@ -1,8 +1,8 @@
-import Image, { StaticImageData } from 'next/image'
+import Image from 'next/image'
 import cx from 'classnames'
 import { PreRenderLinkAsBtn } from '@/components/utils/PreRenderLink'
 import useResponsiveness from '@/lib/useResponsiveness'
-import profilePic from '../../public/profile-pic.webp'
+import profilePic from '@/public/profile-pic.webp'
 import {
     IHeroSection,
     IHeroGradient

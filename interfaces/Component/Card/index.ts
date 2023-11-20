8dd481a5a940f5a247eebf1d1c8ab1b70cac838f
Alex <alex.beciana@gmail.com>
2023-11-19 19:10:37 -0500

    Moved card interfaces to interfaces/card dir

---
+++ interfaces/Component/Card/index.ts
@@ -0,0 +1,62 @@
+import {
+    ITagData,
+    IPostData,
+    IClientData
+} from '@/interfaces'
+
+export interface ITag extends ITagData {
+    addClass?: string;
+}
+
+export interface IProjectCard {
+    slug: string;
+    title: string;
+    status: ITagData;
+    clientType: ITagData;
+    client: IClientData;
+}
+
+export interface IProjectDetailsCard {
+    status: ITag;
+    clientType: ITag;
+    projectTools: ITag[];
+    workAreas: ITag[];
+}
+
+export interface ISkillCard {
+    name: string;
+    image: string;
+}
+
+export interface IBlogPostCard {
+    post: IPostData;
+    imagePriority?: boolean;
+    active: boolean;
+}
+
+export interface IClientCard {
+    name: string;
+    bio: string;
+    link: string;
+    logo: string;
+}
+
+export interface IEduCard {
+    id: number;
+    schoolName: string;
+    schoolWebsite: string;
+    schoolImage: string;
+    achievements: string;
+}
+
+export interface IJobCard {
+    id: number;
+    position: string;
+    startDate: string;
+    companyName: string;
+    companyWebsite: string;
+    companyDescription: string;
+    companyLogo: string;
+    endDate: string;
+    responsibilities: string;
+}

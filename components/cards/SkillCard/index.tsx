fd51838d7a2949f2a08b45bb158f66960e2dfc54
Alex <alex.beciana@gmail.com>
2023-11-19 19:17:05 -0500

    Replaced SkillI with imported ISkillCard

--- components/cards/SkillCard/index.tsx
+++ components/cards/SkillCard/index.tsx
@@ -1,14 +1,10 @@
 import Image from 'next/image'
-
-export interface SkillI {
-    name: string;
-    image: string;
-}
+import { ISkillCard } from '@/interfaces'
 
 const SkillCard = ({
     name,
     image
-}: SkillI | any) => {
+}: ISkillCard) => {
     return (
         <div>
             <div className="h-12 w-12 mx-auto">

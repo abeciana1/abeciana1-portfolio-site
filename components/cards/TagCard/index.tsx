77be433dfb97093c4c5e146dc7b7fc373f490c73
Alex <alex.beciana@gmail.com>
2023-11-22 16:13:26 -0500

    Fixed accessibility of color contrast && Replaced span tag with div && Adjusted styling to increase size, border radius, and font size

--- components/cards/TagCard/index.tsx
+++ components/cards/TagCard/index.tsx
@@ -9,21 +9,21 @@ export const TagCard = ({
     addClass = ""
 }: ITag) => {
     return (
-        <span
+        <div
             key={id}
-            className={cx("my-1 font-medium px-2 rounded-md",{
+            className={cx("py-0.5 font-medium px-2 rounded-md text-md my-0.5",{
                 [addClass]: addClass,
                 ['text-white bg-yellow-700 bg-opacity-60']: color === "brown",
-                ['text-white bg-orange-400 bg-opacity-60']: color === 'orange',
-                ['text-white bg-pink-300']: color === 'pink',
-                ['text-white bg-purple-300']: color === 'purple',
+                ['text-black bg-orange-400 bg-opacity-60']: color === 'orange',
+                ['text-black bg-pink-300']: color === 'pink',
+                ['text-black bg-purple-300']: color === 'purple',
                 ['text-black bg-yellow-200']: color === 'yellow',
-                ['text-white bg-blue-300']: color === 'blue',
+                ['text-black bg-blue-300']: color === 'blue',
                 ['text-black bg-gray-200']: color === 'gray',
                 ['text-black bg-green-400']: color === 'green',
                 ['text-white bg-red-400']: color === 'red',
                 ['text-white bg-blue-800']: color === 'default'
             })}
-        >{tagName}</span>
+        >{tagName}</div>
     )
 }

63feb09ec4870a92954674f818df8d0b3060a032
Alex <alex.beciana@gmail.com>
2023-11-22 17:56:12 -0500

    Added data fetch to cms api for reference data

--- pages/references.tsx
+++ pages/references.tsx
@@ -1,6 +1,8 @@
 import { CustomHead } from '@/components/utils/CustomHead'
+import { gql, GraphQLClient } from 'graphql-request'
+import { IReferenceData } from '@/interfaces'
 
-const ReferencesPage = () => {
+const ReferencesPage = ({ reviews }: {reviews: IReferenceData[]}) => {
 
     return (
         <>
@@ -9,8 +11,38 @@ const ReferencesPage = () => {
                 description="Alex Beciana | References from colleagues, freelance clients, supervisors, and instructors who can attest to my work ethic, competence, and reliability as a developer/engineer and teammate."
                 image="./profile-callout-edited.webp"
             />
+            <h1>What people are saying about me</h1>
         </>
     )
 }
 
 export default ReferencesPage
+
+export const getServerSideProps = async () => {
+
+    const reviewClient = new GraphQLClient(process.env.GRAPH_CMS_API_ENDPOINT || "")
+
+    const reviewQuery = gql`
+        query reviews {
+        testimonials(stage: PUBLISHED) {
+            id
+            reviewerPic {
+            id
+            url(transformation: {document: {output: {format: webp}}})
+            }
+            reviewerName
+            reviewerCompany
+            reviewerPosition
+            testimonialBody
+        }
+        }
+    `
+
+    const {testimonials}: {testimonials: IReferenceData[]} = await reviewClient.request(reviewQuery)
+
+    return {
+        prop: {
+            reviews: testimonials
+        }
+    }
+}

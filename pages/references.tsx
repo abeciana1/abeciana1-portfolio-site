59af966b11031c0116624d38be17cfc4b81ebde6
Alex <alex.beciana@gmail.com>
2023-11-22 16:58:38 -0500

    Created initial render of references page

---
+++ pages/references.tsx
@@ -0,0 +1,16 @@
+import { CustomHead } from '@/components/utils/CustomHead'
+
+const ReferencesPage = () => {
+
+    return (
+        <>
+            <CustomHead
+                title="References"
+                description="Alex Beciana | References from colleagues, freelance clients, supervisors, and instructors who can attest to my work ethic, competence, and reliability as a developer/engineer and teammate."
+                image="./profile-callout-edited.webp"
+            />
+        </>
+    )
+}
+
+export default ReferencesPage

8a0d799d2ed7382fdc06bbaf7e190f459063ff5f
Alex <alex.beciana@gmail.com>
2023-11-22 16:16:10 -0500

    Exported added projectStatusColors obj

--- lib/helper-functions.ts
+++ lib/helper-functions.ts
@@ -3,3 +3,18 @@ export const copyToClipboard = (text: string) => {
         navigator.clipboard.writeText(text)
     }
 }
+
+export const projectStatusColors: {[status: string]: {text: string, color: string}} = {
+    "NotStarted": {
+        text: "Not started",
+        color: "gray"
+    },
+    "InProgress": {
+        text: "In progress",
+        color: "yellow"
+    },
+    "Completed": {
+        text: "Completed",
+        color: "green"
+    }
+}

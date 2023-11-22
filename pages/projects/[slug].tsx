ad8c8ece77b735068df1a3a412090bbb7bb2aa67
Alex <alex.beciana@gmail.com>
2023-11-22 16:20:18 -0500

    Replaced project data fetch with graphQL api && Changed styling && Removed unused imports

--- pages/projects/[slug].tsx
+++ pages/projects/[slug].tsx
@@ -1,75 +1,144 @@
-// import React, { lazy } from 'react'
-// import { GetStaticProps, GetStaticPaths } from 'next'
-// import {
-//     getProjectSlug,
-//     getClientInfo,
-//     getProjectTitle,
-//     getProjectStatus,
-//     getClientType,
-//     getProjectTools,
-//     getProjectWorkAreas
-// } from '../../lib/notion-proj-props'
-// import ClientCard from '../../components/cards/ClientCard'
-// import ProjectDetailsCard from '../../components/cards/ProjectDetailsCard'
-// import { ScrollToTopBtn } from '../../components/utils/_buttons'
-// import { BlogPostHead } from '../../components/utils/CustomHead'
-// import { ITag } from '@/interfaces'
+import React from 'react'
+import { GetStaticProps, GetStaticPaths } from 'next'
+import Image from 'next/image'
+import { ScrollToTopBtn } from '@/components/utils/_buttons'
+import { BlogPostHead } from '@/components/utils/CustomHead'
+import { ITag, IPaths, IParams, IProject } from '@/interfaces'
+import { gql, GraphQLClient } from 'graphql-request'
+import Markdown from 'react-markdown'
+import gfm from 'remark-gfm'
+import { TagCard } from '@/components/cards/TagCard'
+import { projectStatusColors } from '@/lib/helper-functions'
 
-// const PortfolioProjectPage = ({ project, blocks }: any) => {
+const PortfolioProjectPage = ({ project }: {project: IProject}) => {
 
-//     return (
-//         <React.Fragment>
-//             <BlogPostHead
-//                 title={`Project | ${title}`}
-//                 description={bio}
-//                 article={{
-//                     blogTags: projectTools.map((tool: ITag) => tool.tagName),
-//                 }}
-//                 image={logo}
-//             />
-//             <h1 className="mt-5 text-5xl font-reross leading-relaxed">{title}</h1>
-//             <section className="flex flex-col md:flex-row md:space-x-10">
-//                 <aside className="mt-16 md:basis-1/4 min-w-xl">
-//                     <ClientCard
-//                         name={name}
-//                         bio={bio}
-//                         logo={logo}
-//                         link={link}
-//                     />
-//                     <ProjectDetailsCard
-//                         status={status}
-//                         clientType={clientType}
-//                         projectTools={projectTools}
-//                         workAreas={workAreas}
-//                     />
-//                 </aside>
-//                 <section
-//                     className="py-4 break-words md:basis-3/4 max-w-md lg:max-w-2xl"
-//                 >
-                    
-//                 </section>
-//                 <ScrollToTopBtn/>
-//             </section>
-//         </React.Fragment>
-//     )
-// }
+    return (
+        <React.Fragment>
+            <BlogPostHead
+                title={`Project | ${project.projectTitle}`}
+                description={project.excerpt}
+                article={{
+                    blogTags: project.projectTags.map((tool: ITag) => tool.tagName),
+                }}
+                image={project.featuredImage.url}
+            />
+            <h1 className="mt-5 text-5xl font-reross leading-relaxed">{project.projectTitle}</h1>
+            <section className="flex flex-col-reverse md:flex-row md:space-x-10">
+            <section
+                    className="py-4 break-words md:basis-3/4 max-w-md lg:max-w-2xl leading-loose text-xl"
+                >
+                    <Markdown
+                        children={project.projectDescription}
+                        remarkPlugins={[gfm]}
+                        components={{
+                            input(props: any) {
+                                const { node } = props
+                                return (
+                                    <input type="checkbox" readOnly={true} checked={node.properties.checked} />
+                                )
+                            }
+                        }}
+                    />
+                </section>
+                <section className="md:basis-1/4 min-w-xl">
+                    <Image
+                        src={project.featuredImage.url}
+                        width={300}
+                        height={300}
+                        alt={`Alex Beciana | Portfolio Project | ${project.projectTitle}`}
+                        className='mx-auto'
+                    />
+                    <div className="flex items-center py-5">
+                            <span className="font-semiBold">Project status: </span>
+                            <TagCard
+                                id={project.id}
+                                color={projectStatusColors?.[`${project.projectStatus}`]?.color}
+                                tagName={projectStatusColors?.[`${project.projectStatus}`]?.text}
+                                addClass="ml-1"
+                            />
+                    </div>
+                    {project.projectTags && project.projectTags.length > 0 &&
+                        <>
+                            <div className="text-lg font-semiBold">What I used:</div>
+                            <ul className="list-none flex flex-wrap space-x-1">
+                                {project.projectTags.map((tag: ITag) => {
+                                    return (
+                                        <li key={tag.id}><TagCard color={tag.color} tagName={tag.tagName} /></li>
+                                    )
+                                })}
+                            </ul>
+                        </>
+                    }
+                </section>
+                <ScrollToTopBtn/>
+            </section>
+        </React.Fragment>
+    )
+}
 
-// export default PortfolioProjectPage
+export default PortfolioProjectPage
 
-// export const getStaticPaths: GetStaticPaths = async (context: any) => {
-//     const paths = []
+export const getStaticPaths: GetStaticPaths = async () => {
+    const projClient = new GraphQLClient(process.env.GRAPH_CMS_API_ENDPOINT || "")
+
+    const projQuery = gql`
+        query projects {
+        projects(stage: PUBLISHED) {
+            slug
+        }
+        }
+    `
+
+    const projPaths: IPaths = await projClient.request(projQuery)
+
+    const paths = projPaths.projects.map((proj: IParams) => {
+        return {
+            params: {
+                slug: proj.slug
+            }
+        }
+    })
     
-//     return {
-//         paths,
-//         fallback: false
-//     }
-// }
+    return {
+        paths,
+        fallback: false
+    }
+}
 
-// export const getStaticProps: GetStaticProps = async (context: any) => {
+export const getStaticProps: GetStaticProps = async (context) => {
+    const {slug} = context.params as IParams
 
-//     return {
-//         props: {
-//         },
-//         revalidate: 600
-//     }
-// }
+    const projectQuery = gql`
+        query projects {
+        projects(stage: PUBLISHED, where: {slug: "${slug}"}) {
+            excerpt
+            projectDescription
+            projectLink
+            projectLinks
+            projectStatus
+            projectTitle
+            slug
+            featuredImage {
+            url(transformation: {document: {output: {format: webp}}})
+            }
+            id
+            projectTags {
+                color
+                id
+                tagName
+            }
+        }
+        }
+    `
+
+const projClient = new GraphQLClient(process.env.GRAPH_CMS_API_ENDPOINT || "")
+
+const projData: any = await projClient.request(projectQuery)
+
+    return {
+        props: {
+            project: projData.projects[0]
+        },
+        revalidate: 60 * 60 * 10
+    }
+}

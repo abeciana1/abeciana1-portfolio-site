0712434f30ce3719ffe75d5888802b9be22224d3
Alex <alex.beciana@gmail.com>
2023-11-21 21:53:36 -0500

    Commented out pages until next iteration to redo

--- pages/portfolio/[slug].tsx
+++ pages/portfolio/[slug].tsx
@@ -1,112 +1,75 @@
-import React, { lazy } from 'react'
-import { GetStaticProps, GetStaticPaths } from 'next'
-import { getDatabase } from '../../lib/helper-functions'
-import {
-    getProjectSlug,
-    getClientInfo,
-    getProjectTitle,
-    getProjectStatus,
-    getClientType,
-    getProjectTools,
-    getProjectWorkAreas
-} from '../../lib/notion-proj-props'
-import { BlockMapType } from "react-notion";
-import ClientCard from '../../components/cards/ClientCard'
-import ProjectDetailsCard from '../../components/cards/ProjectDetailsCard'
-import { ScrollToTopBtn } from '../../components/utils/_buttons'
-const NotionContentRender = lazy(() => import('../../components/notion-comps'))
-import { BlogPostHead } from '../../components/utils/CustomHead'
-import { TagI } from '../../components/cards/TagCard'
+// import React, { lazy } from 'react'
+// import { GetStaticProps, GetStaticPaths } from 'next'
+// import {
+//     getProjectSlug,
+//     getClientInfo,
+//     getProjectTitle,
+//     getProjectStatus,
+//     getClientType,
+//     getProjectTools,
+//     getProjectWorkAreas
+// } from '../../lib/notion-proj-props'
+// import ClientCard from '../../components/cards/ClientCard'
+// import ProjectDetailsCard from '../../components/cards/ProjectDetailsCard'
+// import { ScrollToTopBtn } from '../../components/utils/_buttons'
+// import { BlogPostHead } from '../../components/utils/CustomHead'
+// import { ITag } from '@/interfaces'
 
-const PortfolioProjectPage = ({ project, blocks }: any) => {
+// const PortfolioProjectPage = ({ project, blocks }: any) => {
 
-    const client = getClientInfo(project)
-    const title = getProjectTitle(project)
-    const status = getProjectStatus(project)
-    const clientType = getClientType(project)
-    const projectTools = getProjectTools(project)
-    const workAreas = getProjectWorkAreas(project)
+//     return (
+//         <React.Fragment>
+//             <BlogPostHead
+//                 title={`Project | ${title}`}
+//                 description={bio}
+//                 article={{
+//                     blogTags: projectTools.map((tool: ITag) => tool.tagName),
+//                 }}
+//                 image={logo}
+//             />
+//             <h1 className="mt-5 text-5xl font-reross leading-relaxed">{title}</h1>
+//             <section className="flex flex-col md:flex-row md:space-x-10">
+//                 <aside className="mt-16 md:basis-1/4 min-w-xl">
+//                     <ClientCard
+//                         name={name}
+//                         bio={bio}
+//                         logo={logo}
+//                         link={link}
+//                     />
+//                     <ProjectDetailsCard
+//                         status={status}
+//                         clientType={clientType}
+//                         projectTools={projectTools}
+//                         workAreas={workAreas}
+//                     />
+//                 </aside>
+//                 <section
+//                     className="py-4 break-words md:basis-3/4 max-w-md lg:max-w-2xl"
+//                 >
+                    
+//                 </section>
+//                 <ScrollToTopBtn/>
+//             </section>
+//         </React.Fragment>
+//     )
+// }
 
-    const {
-        name,
-        bio,
-        logo,
-        link
-    } = client
+// export default PortfolioProjectPage
 
-    return (
-        <React.Fragment>
-            <BlogPostHead
-                title={`Project | ${title}`}
-                description={bio}
-                article={{
-                    blogTags: projectTools.map((tool: TagI) => tool.name),
-                }}
-                image={logo}
-            />
-            <h1 className="mt-5 text-5xl font-reross leading-relaxed">{title}</h1>
-            <section className="flex flex-col md:flex-row md:space-x-10">
-                <aside className="mt-16 md:basis-1/4 min-w-xl">
-                    <ClientCard
-                        name={name}
-                        bio={bio}
-                        logo={logo}
-                        link={link}
-                    />
-                    <ProjectDetailsCard
-                        status={status}
-                        clientType={clientType}
-                        projectTools={projectTools}
-                        workAreas={workAreas}
-                    />
-                </aside>
-                <section
-                    className="py-4 break-words md:basis-3/4 max-w-md lg:max-w-2xl"
-                >
-                    <NotionContentRender blocks={blocks} />
-                </section>
-                <ScrollToTopBtn/>
-            </section>
-        </React.Fragment>
-    )
-}
-
-export default PortfolioProjectPage
-
-export const getStaticPaths: GetStaticPaths = async (context: any) => {
-    const projects = await getDatabase(process.env.NOTION_PORTFOLIO_DATABASE_ID)
-    let paths = projects.map((project: any) => {
-        let slug = getProjectSlug(project.properties)
-        return {
-            params: {
-                slug: slug
-            }
-        }
-    })
+// export const getStaticPaths: GetStaticPaths = async (context: any) => {
+//     const paths = []
     
-    return {
-        paths,
-        fallback: false
-    }
-}
+//     return {
+//         paths,
+//         fallback: false
+//     }
+// }
 
-export const getStaticProps: GetStaticProps = async (context: any) => {
-    const projects = await getDatabase(process.env.NOTION_PORTFOLIO_DATABASE_ID)
+// export const getStaticProps: GetStaticProps = async (context: any) => {
 
-    const project = projects.find((proj: any) => {
-        let slug = getProjectSlug(proj.properties)
-        if (slug === context.params.slug) {
-            return proj
-        }
-    })
-
-    const blocks: BlockMapType = await fetch(`https://notion-api.splitbee.io/v1/page/${project.id}`).then((res) => res.json());
-
-    return {
-        props: {
-            project: project.properties,
-            blocks
-        },
-        revalidate: 600
-    }
-}
+//     return {
+//         props: {
+//         },
+//         revalidate: 600
+//     }
+// }
--- pages/portfolio/index.tsx
+++ pages/portfolio/index.tsx
@@ -1,50 +1,49 @@
-import React from 'react'
-import { GetStaticProps } from 'next'
-import { CustomHead } from '../../components/utils/CustomHead'
-import { getDatabase } from '../../lib/helper-functions'
-import dynamic from 'next/dynamic';
+// import React from 'react'
+// import { GetStaticProps } from 'next'
+// import { CustomHead } from '@/components/utils/CustomHead'
+// import dynamic from 'next/dynamic';
 
-const ProjectCard = dynamic(() => import('../../components/cards/ProjectCard'), {
-    ssr: false
-})
+// const ProjectCard = dynamic(() => import('@/components/cards/ProjectCard'), {
+//     ssr: false
+// })
 
-const PortfolioPage = ({
-    projects
-}: any) => {
+// const PortfolioPage = ({
+//     projects
+// }: any) => {
 
-    return (
-        <React.Fragment>
-            <CustomHead
-                title="Portfolio"
-                description="Alex Beciana | Creating projects for myself, friends, and clients."
-            />
-            <>
-                <h2
-                    className="text-4xl leading-relaxed"
-                >Portfolio projects</h2>
-                <section
-                    className="py-12 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-12"
-                >
-                    {projects.map((project: any) => {
-                        return <ProjectCard key={project.id} project={project?.properties} />
-                    })}
-                </section>
-            </>
-        </React.Fragment>
-    )
-}
+//     return (
+//         <React.Fragment>
+//             <CustomHead
+//                 title="Portfolio"
+//                 description="Alex Beciana | Creating projects for myself, friends, and clients."
+//             />
+//             <>
+//                 <h2
+//                     className="text-4xl leading-relaxed"
+//                 >Portfolio projects</h2>
+//                 <section
+//                     className="py-12 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-12"
+//                 >
+//                     {projects.map((project: any) => {
+//                         return <ProjectCard key={project.id} project={project?.properties} />
+//                     })}
+//                 </section>
+//             </>
+//         </React.Fragment>
+//     )
+// }
 
-export const getStaticProps: GetStaticProps = async () => {
+// export const getStaticProps: GetStaticProps = async () => {
 
-    const response = await getDatabase(process.env.NOTION_PORTFOLIO_DATABASE_ID)
+//     const response = await getDatabase(process.env.NOTION_PORTFOLIO_DATABASE_ID)
 
-    return {
-        props: {
-            projects: response
-        },
-        revalidate: 600
-    }
-}
+//     return {
+//         props: {
+//             projects: response
+//         },
+//         revalidate: 600
+//     }
+// }
 
 
-export default PortfolioPage
+// export default PortfolioPage

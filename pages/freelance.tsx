2d7ead755a8d76a9aa50c229d492178385c1c3df
Alex <alex.beciana@gmail.com>
2023-11-23 15:02:16 -0500

    Imported new icons and Added services

--- pages/freelance.tsx
+++ pages/freelance.tsx
@@ -12,11 +12,22 @@ import Clients from '@/data/clients.json'
 import Logo from '@/components/cards/Logo'
 import { ILogo } from '@/interfaces'
 import { TwoColumnGrid, ThreeColumnGrid } from '@/components/layouts'
+import { CustomHead } from '@/components/utils/CustomHead'
+import { RiAspectRatioFill } from "react-icons/ri";
+import { FaGear } from "react-icons/fa6";
+import { BiSolidBookContent } from "react-icons/bi";
+import { IoStorefrontSharp } from "react-icons/io5";
+import { PiEngineFill } from "react-icons/pi";
+
 
 const FreelancePage = (): JSX.Element => {
 
     return (
         <React.Fragment>
+            <CustomHead
+                title="AB Creative Freelancing"
+                description="Alex Beciana | Empower your brand online with top-notch web and app development services. Elevate user experience and drive success with our expert solutions."
+            />
             <HeroSectionWithLinkGradientBG
                 heading="Let's work together"
                 taglineBody="Experienced frontend developer and software engineer. Offering services in web and software development, digital marketing, and community management. Expert at working across technical and non-technical teams."
@@ -51,24 +62,40 @@ const FreelancePage = (): JSX.Element => {
                 <h2 className="text-4xl text-center relative z-50">My offerings</h2>
                 <TwoColumnGrid addClass="mt-20 gap-20">
                     <ServiceIcon
-                        icon={Engineering}
-                        text="Specializing frontend development services that deliver clean and efficient code for web applications and sites. With my expertise, I can provide you with a modern, accessible, SEO-friendly, and responsive user interface that enhances user experience. Timely delivery within your budget. Ongoing support is also available."
+                        title='Frontend Development'
+                        icon={IoStorefrontSharp}
+                        text="Craft visually stunning and intuitive user interfaces, ensuring a seamless and enjoyable user experience on your websites and applications."
+                        color="red"
+                    />
+                    <ServiceIcon
+                        title='Responsive Website Design'
+                        icon={RiAspectRatioFill}
+                        text="Craft visually appealing and user-friendly websites that adapt seamlessly to various devices, ensuring an optimal viewing experience."
+                        color="red"
+                    />
+                    <ServiceIcon
+                        title='Content Management Systems (CMS)'
+                        icon={BiSolidBookContent}
+                        text="Implement user-friendly CMS platforms, empowering clients to easily manage and update their website content without technical expertise."
                         color="blue"
                     />
                     <ServiceIcon
-                        icon={Engineering}
-                        text="Specializing frontend development services that deliver clean and efficient code for web applications and sites. With my expertise, I can provide you with a modern, accessible, SEO-friendly, and responsive user interface that enhances user experience. Timely delivery within your budget. Ongoing support is also available."
+                        title='API Integration Services'
+                        icon={FaGear}
+                        text="Enhance functionality and connectivity by integrating third-party APIs, ensuring seamless communication between different software applications for improved performance."
                         color="blue"
                     />
                     <ServiceIcon
-                        icon={Engineering}
-                        text="Specializing frontend development services that deliver clean and efficient code for web applications and sites. With my expertise, I can provide you with a modern, accessible, SEO-friendly, and responsive user interface that enhances user experience. Timely delivery within your budget. Ongoing support is also available."
-                        color="blue"
+                        title='Backend Development'
+                        icon={PiEngineFill}
+                        text="Power your applications with robust and scalable backend systems. From database management to server-side logic, we optimize functionality, ensuring seamless performance and data integrity."
+                        color="yellow"
                     />
                     <ServiceIcon
-                        icon={Engineering}
-                        text="Specializing frontend development services that deliver clean and efficient code for web applications and sites. With my expertise, I can provide you with a modern, accessible, SEO-friendly, and responsive user interface that enhances user experience. Timely delivery within your budget. Ongoing support is also available."
-                        color="blue"
+                        title='Performance Optimization'
+                        icon={PiEngineFill}
+                        text="Enhance the speed and efficiency of your digital assets. Our performance optimization services focus on streamlining code, improving server response times, and optimizing database queries to ensure your websites and applications deliver a lightning-fast and responsive user experience."
+                        color="yellow"
                     />
                 </TwoColumnGrid>
             </section>

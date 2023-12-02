93a90103b45a8a55ddef4c0eb263cee636bfb0ca
Alex <alex.beciana@gmail.com>
2023-12-02 15:42:20 -0500

    Created next config case for production and development builds

--- next.config.js
+++ next.config.js
@@ -11,43 +11,86 @@ const millionConfig = {
   auto: true,
 }
 
-module.exports = million.next(withBundleAnalyzer({
-    reactStrictMode: true,
-    swcMinify: true,
-    experimental: {
-      nextScriptWorkers: true,
+if (process.env.NODE_ENV === 'production') {
+  module.exports = million.next(withBundleAnalyzer({
+      reactStrictMode: true,
+      swcMinify: true,
+      experimental: {
+        nextScriptWorkers: true,
+      },
+      images: {
+        deviceSizes: [100, 200, 320, 375, 425, 640, 750, 768, 828, 1024, 1080, 1200, 1920, 2048, 3840],
+        imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
+        remotePatterns: [
+          {
+            protocol: "https",
+            hostname: "media.graphcms.com/**",
+          },
+          {
+            protocol: "https",
+            hostname: "s3.us-west-2.amazonaws.com/**"
+          },
+          {
+            protocol: "https",
+            hostname: "www.notion.so/**"
+          },
+          {
+            protocol: "https",
+            hostname: "media.graphassets.com"
+          }
+        ],
+        dangerouslyAllowSVG: true,
+        contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
     },
-    images: {
-      deviceSizes: [100, 200, 320, 375, 425, 640, 750, 768, 828, 1024, 1080, 1200, 1920, 2048, 3840],
-      imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
-      remotePatterns: [
+    async redirects() {
+      return [
         {
-          protocol: "https",
-          hostname: "media.graphcms.com/**",
-        },
-        {
-          protocol: "https",
-          hostname: "s3.us-west-2.amazonaws.com/**"
-        },
-        {
-          protocol: "https",
-          hostname: "www.notion.so/**"
-        },
-        {
-          protocol: "https",
-          hostname: "media.graphassets.com"
+          source: "/portfolio",
+          destination: "/freelance",
+          permanent: true
         }
-      ],
-      dangerouslyAllowSVG: true,
-      contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
-  },
-  async redirects() {
-    return [
-      {
-        source: "/portfolio",
-        destination: "/freelance",
-        permanent: true
-      }
-    ]
-  }
-}, millionConfig))
+      ]
+    }
+  }, millionConfig))
+} else {
+    module.exports = withBundleAnalyzer({
+      reactStrictMode: true,
+      swcMinify: true,
+      experimental: {
+        nextScriptWorkers: true,
+      },
+      images: {
+        deviceSizes: [100, 200, 320, 375, 425, 640, 750, 768, 828, 1024, 1080, 1200, 1920, 2048, 3840],
+        imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
+        remotePatterns: [
+          {
+            protocol: "https",
+            hostname: "media.graphcms.com/**",
+          },
+          {
+            protocol: "https",
+            hostname: "s3.us-west-2.amazonaws.com/**"
+          },
+          {
+            protocol: "https",
+            hostname: "www.notion.so/**"
+          },
+          {
+            protocol: "https",
+            hostname: "media.graphassets.com"
+          }
+        ],
+        dangerouslyAllowSVG: true,
+        contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
+    },
+    async redirects() {
+      return [
+        {
+          source: "/portfolio",
+          destination: "/freelance",
+          permanent: true
+        }
+      ]
+    }
+  })
+}
